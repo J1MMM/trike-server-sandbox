bb9f6e500838fd138715f704ab8bbaa82a3e1084
J1MMM <jimuelbaraero00@gmail.com>
2024-02-26 05:40:01 +0800

    2/26/24

--- controllers/franchiseController.js
+++ controllers/franchiseController.js
@@ -2,9 +2,11 @@ const Franchise = require('../model/Franchise')
 
 const getAllData = async(req, res) => {
   try {
-      const result = await Franchise.find().exec()
+      const result = await Franchise.find()
+      console.log(result)
       res.json(result)
   } catch (err) {
+    console.log(err);
     res.status(500).json({ "message": err.message })
 
   }
