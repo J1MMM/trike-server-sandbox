15247628d2c289c04dc55762824edb3e85b0346c
J1MMM <jimuelbarero00@gmail.com>
2024-04-11 09:26:55 +0800

    april 11 2024

--- model/User.js
+++ model/User.js
@@ -1,70 +1,71 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-// Teacher: 1984        
+// Teacher: 1984
 // Admin: 5150
 
 const userSchema = new Schema({
-    firstname: {
-        type: String,
-        required: true
-
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  middlename: String,
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  refreshToken: String,
+  roles: {
+    SuperAdmin: {
+      type: Number,
+      default: 0,
     },
-    lastname: {
-        type: String,
-        required: true
-
+    Admin: {
+      type: Number,
+      default: 0,
     },
-    middlename: String,
-    password: {
-        type: String,
-        required: true
-
+    CTMO1: {
+      type: Number,
+      default: 0,
     },
-    email: {
-        type: String,
-        required: true
+    CTMO2: {
+      type: Number,
+      default: 0,
     },
-    refreshToken: String,
-    roles: {
-        SuperAdmin: {
-            type: Number,
-            default: 0
-        },
-        Admin: {
-            type: Number,
-            default: 0
-        },
-        CTMO1: {
-            type: Number,
-            default: 0
-        },
-        CTMO2: {
-            type: Number,
-            default: 0
-        },
-        CTMO3: {
-            type: Number,
-            default: 0
-        },
+    CTMO3: {
+      type: Number,
+      default: 0,
     },
-    gender: {
-        type: String,
-        require: true
+    Cashier: {
+      type: Number,
+      default: 0,
     },
-    address: {
-        type: String,
-        require: true
-    },
-    contactNo: {
-        type: String,
-        require: true
-    },
-    archive: {
-        type: Boolean,
-        require: true,
-        default: false
-    }
+  },
+  gender: {
+    type: String,
+    require: true,
+  },
+  address: {
+    type: String,
+    require: true,
+  },
+  contactNo: {
+    type: String,
+    require: true,
+  },
+  archive: {
+    type: Boolean,
+    require: true,
+    default: false,
+  },
 });
 
-module.exports = mongoose.model('User', userSchema)
+module.exports = mongoose.model("User", userSchema);
