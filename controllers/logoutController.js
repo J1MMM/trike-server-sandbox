1d3215d993c59b83b6d0b7fb09be28c4f456f9d6
J1MMM <jimuelbarero00@gmail.com>
2024-04-22 16:06:30 +0800

    april 22 2024

--- controllers/logoutController.js
+++ controllers/logoutController.js
@@ -1,23 +1,22 @@
 const User = require("../model/User");
 
-
 const handleLogout = async (req, res) => {
-    const cookies = req.cookies
-    if (!cookies?.jwt) return res.sendStatus(204)
+  const cookies = req.cookies;
+  if (!cookies?.jwt) return res.sendStatus(204);
 
-    const refreshToken = cookies.jwt;
-    const foundUser = await User.findOne({ refreshToken }).exec()
-    if (!foundUser) {
-        res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true })
-        return res.sendStatus(204);
-    }
+  const refreshToken = cookies.jwt;
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  if (!foundUser) {
+    res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true });
+    return res.sendStatus(204);
+  }
 
-    //delete refresh token in the database
-    foundUser.refreshToken = "";
-    const result = await foundUser.save()
+  //delete refresh token in the database
+  foundUser.refreshToken = "";
+  const result = await foundUser.save();
 
-    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true }) //secure: true - only serves on htttps
-    res.sendStatus(204);
-}
+  res.clearCookie("jwt", { httpOnly: true, sameSite: "None", secure: true }); //secure: true - only serves on htttps
+  res.sendStatus(204);
+};
 
-module.exports = { handleLogout }
+module.exports = { handleLogout };
